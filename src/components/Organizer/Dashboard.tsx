1351cf4312906c4d55d8ef8263e9bf5156140b57
Jeff Ma <jucjeff96@gmail.com>
2024-12-06 13:23:51 -0600

    refactor: refactored organizer dashboard display logic

--- src/components/Organizer/Dashboard.tsx
+++ src/components/Organizer/Dashboard.tsx
@@ -1,32 +1,45 @@
 import { Card, CardBody, Tab, Tabs } from "@nextui-org/react";
 
+import useAuth from "../../hooks/authHooks";
+
+import { LoginForms } from "../LoginForm";
 import Profile from "./Profile";
 import Template from "./Template";
 
 export default function Dashboard() {
+  const user = useAuth();
+
   return (
     <div className="flex-col items-center px-4 max-sm:px-0">
-      <Tabs variant="light" aria-label="Organizer Dashboard Tabs">
-        <Tab key="events" title="Events">
-          <Card>
-            <CardBody className="flex items-center">Content</CardBody>
-          </Card>
-        </Tab>
-        <Tab className="flex justify-center" key="templates" title="Templates">
-          <Card className="w-[540px] px-5 max-sm:w-full">
-            <CardBody>
-              <Template />
-            </CardBody>
-          </Card>
-        </Tab>
-        <Tab key="profile" title="Profile">
-          <Card>
-            <CardBody className="flex items-center">
-              <Profile />
-            </CardBody>
-          </Card>
-        </Tab>
-      </Tabs>
+      {user ? (
+        <Tabs variant="light" aria-label="Organizer Dashboard Tabs">
+          <Tab key="events" title="Events">
+            <Card>
+              <CardBody className="flex items-center">Content</CardBody>
+            </Card>
+          </Tab>
+          <Tab
+            className="flex justify-center"
+            key="templates"
+            title="Templates"
+          >
+            <Card className="w-[540px] px-5 max-sm:w-full">
+              <CardBody>
+                <Template />
+              </CardBody>
+            </Card>
+          </Tab>
+          <Tab key="profile" title="Profile">
+            <Card>
+              <CardBody className="flex items-center">
+                <Profile />
+              </CardBody>
+            </Card>
+          </Tab>
+        </Tabs>
+      ) : (
+        <LoginForms />
+      )}
     </div>
   );
 }
