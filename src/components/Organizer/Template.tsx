eee6e959ca18208c1651f7a4388d47dc71edd15a
Jeff Ma <jucjeff96@gmail.com>
2024-12-06 13:37:00 -0600

    feat: added additional input fields to event form

--- src/components/Organizer/Template.tsx
+++ src/components/Organizer/Template.tsx
@@ -3,10 +3,17 @@ import { useState } from "react";
 import { CheckboxGroup, Checkbox } from "@nextui-org/checkbox";
 import { Input, Textarea } from "@nextui-org/input";
 import { Button, DateInput, TimeInput } from "@nextui-org/react";
-
+import ImageUploading, { ImageListType } from "react-images-uploading";
 import { useForm, SubmitHandler } from "react-hook-form";
 
+import { musicList } from "../../data";
+
 import { EventFormTemplateInputs } from "../../types";
+
+interface Image {
+  data_url: string; // Base64 image data
+  file: File; // The image file
+}
 
 export default function Template() {
   const {
@@ -22,7 +29,51 @@ export default function Template() {
     reset();
   };
 
+  const [images, setImages] = useState<Image[]>([]);
+  const [isFreeEvent, setIsFreeEvent] = useState<boolean>(false);
   const [selected, setSelected] = useState(["bachata", "salsa"]);
+
+  // Store the numeric input for each checkbox (e.g., bachata: 20, salsa: 30, etc.)
+  const [additionalData, setAdditionalData] = useState<{
+    [key: string]: number;
+  }>({
+    bachata: 40,
+    salsa: 40,
+    merengue: 0,
+    reggaeton: 0,
+    others: 0,
+  });
+
+  const onChange = (imageList: ImageListType, addUpdateIndex?: number[]) => {
+    // data for submit
+    console.log(imageList, addUpdateIndex);
+    setImages(imageList as Image[]);
+  };
+
+  const handleFreeEvent = () => {
+    setIsFreeEvent(!isFreeEvent);
+  };
+
+  // Handle checkbox change (add/remove checkbox from selected list)
+  const handleCheckboxChange = (value: string) => {
+    setSelected((prevSelected) =>
+      prevSelected.includes(value)
+        ? prevSelected.filter((item) => item !== value)
+        : [...prevSelected, value]
+    );
+  };
+
+  // Handle numeric input change
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    value: string
+  ) => {
+    const newValue = Math.max(0, Math.min(100, Number(event.target.value))); // Ensure the value is between 0 and 100
+    setAdditionalData((prevData) => ({
+      ...prevData,
+      [value]: newValue,
+    }));
+  };
 
   return (
     <form className="grid-rows grid gap-4" onSubmit={handleSubmit(onSubmit)}>
@@ -33,34 +84,101 @@ export default function Template() {
         {...register("title")}
         isClearable
       />
+      <div>
+        <ImageUploading
+          multiple={false} // Only allow 1 image
+          value={images}
+          onChange={onChange}
+          dataURLKey="data_url"
+        >
+          {({
+            imageList,
+            onImageUpload,
+            onImageUpdate,
+            onImageRemove,
+            isDragging,
+            dragProps,
+          }) => (
+            // write your building UI
+            <div className="upload__image-wrapper">
+              <button
+                className="hover:text-primary"
+                style={isDragging ? { color: "lime" } : undefined}
+                onClick={onImageUpload}
+                {...dragProps}
+              >
+                Click or Drop image here
+              </button>
+              &nbsp;
+              {imageList.map((image, index) => (
+                <div key={index} className="image-item">
+                  <img src={image["data_url"]} alt="" width="540" />
+                  <div className="image-item__btn-wrapper">
+                    <button onClick={() => onImageUpdate(index)}>Update</button>
+                    <button onClick={() => onImageRemove(index)}>Remove</button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          )}
+        </ImageUploading>
+      </div>
       <Input type="text" label="Location" />
       <DateInput label={"Event date"} />
       <TimeInput label="Event Start Time" />
       <TimeInput label="Event End Time" />
-      <Input
-        type="text"
-        label="Price"
-        {...register("price", { required: true })}
-      />
       <Textarea
         className="w-full"
         label="Description"
         {...register("description", { required: "Description is required" })}
         placeholder="Enter your description"
       />
+      <div>
+        {!isFreeEvent && (
+          <Input
+            type="text"
+            label="Price"
+            {...register("price", { required: true })}
+          />
+        )}
+        <Checkbox className="mt-1" color="secondary" onClick={handleFreeEvent}>
+          <p className="hover:text-primary">This is a free event</p>
+        </Checkbox>
+      </div>
       <CheckboxGroup
         label="Select music"
         color="secondary"
         value={selected}
-        onValueChange={setSelected}
+        onValueChange={(newSelected) => setSelected(newSelected as string[])}
       >
-        <Checkbox value="bachata">Bachata</Checkbox>
-        <Checkbox value="salsa">Salsa</Checkbox>
-        <Checkbox value="merengue">Merengue</Checkbox>
-        <Checkbox value="reggaeton">Reggaeton</Checkbox>
-        <Checkbox value="others">Others</Checkbox>
+        {musicList.map((genre) => (
+          <div className="flex" key={genre}>
+            <div className="flex w-1/3">
+              <Checkbox value={genre}>
+                {genre.charAt(0).toUpperCase() + genre.slice(1)}
+              </Checkbox>
+            </div>
+            {selected.includes(genre) && (
+              <Input
+                className="ms-4"
+                variant="bordered"
+                size="sm"
+                type="number"
+                min={1}
+                max={100}
+                aria-label={`${genre} percentage`}
+                label="Percentage"
+                value={String(additionalData[genre] || 0)}
+                onChange={(e) => handleInputChange(e, genre)}
+              />
+            )}
+          </div>
+        ))}
       </CheckboxGroup>
-      <Button className="mb-4 bg-primary text-gray-800" type="submit">
+      <Button
+        className="mb-4 hover:bg-primary hover:text-gray-800"
+        type="submit"
+      >
         Submit
       </Button>
     </form>
